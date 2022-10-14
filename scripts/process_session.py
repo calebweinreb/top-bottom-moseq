c5153f4e27b0cdc1008078d23923dfe33b732e20
Jonah Pearl <jonahpearl@g.harvard.edu>
2022-10-14 11:38:19 -0400

    add weights to package

--- scripts/process_session.py
+++ scripts/process_session.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 import tqdm
 import pickle
 import os
+from os.path import join, exists
 import click
 
+import top_bottom_moseq
 from top_bottom_moseq.orthographic import *
 from top_bottom_moseq.util import load_matched_frames, load_intrinsics, load_config
 from top_bottom_moseq.segmentation import segment_session
 from top_bottom_moseq.inpainting import inpaint_session
 from top_bottom_moseq.dim_reduction import encode_session
 from top_bottom_moseq.qc import save_qc_movie, grab_qc_frame
+
+PATH = top_bottom_moseq.__path__
 
 @click.command()
 @click.argument('prefix')
@@ -29,10 +33,10 @@ def main(prefix, config_filepath, qc_vid, qc_vid_len):
     transforms = pickle.load(open(config['transforms_path'], 'rb'))
 
     # run the pipeline
-    segment_session(prefix, config['mouse_segmentation_weights'], config['occlusion_segmentation_weights'])  
+    segment_session(prefix, join(PATH, config['mouse_segmentation_weights']), join(PATH, config['occlusion_segmentation_weights']))  
     orthographic_reprojection(prefix, transforms, intrinsics)
-    inpaint_session(prefix, config['inpainting_weights'])
-    encode_session(prefix, config['autoencoder_weights'], config['localization_weights'])
+    inpaint_session(prefix, join(PATH, config['inpainting_weights']))
+    encode_session(prefix, join(PATH, config['autoencoder_weights']), join(PATH, config['localization_weights']))
 
     if qc_vid:
         save_qc_movie(prefix, qc_vid_len)
